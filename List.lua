270244d8b2a62895e9ba336f2db242c84b0c68c0
Severity <system0464@gmail.com>
2025-04-19 09:02:53 +0300

    Update List.lua

--- List.lua
+++ List.lua
@@ -1,1 +1,9 @@
-
+	return {
+							["Severity_svc"] = {
+								Combined = "93515FAE91216EBCDD599EC83265B2_E234A003-18E6-4546-996E-CC216EEDAC75",
+								MixedId = "2EB253642A3080C3E-91985ED6D-C4B5E4661-291996EEA-FC5C1251369EEDAC75",
+								Rank = "Owner | Lead Dev",
+								RankColor = Color3.fromRGB(255, 207, 255),
+								License = "Valid_!92",
+							},
+						}
