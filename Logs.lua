e45810ebd5a16e4ff104be78881e02125b9af369
Severity <system0464@gmail.com>
2025-05-10 18:11:05 +0300

    Update Logs.lua

--- Logs.lua
+++ Logs.lua
@@ -1,1 +1,25 @@
-return {}
+return {
+			["Release - version 1.0.0"] = {
+				Date = "25 | 04 | 2025",
+				Header = "",
+				Content = [[
+[+] Changed MainFrame's Transparency 
+[+] Changed Font to Inter SemiBold (inspired by maclib)
+[+] Changed Keysystem Sizing and Exit Animation
+[+] Changed Notification Sizing And Positioning
+		]],
+
+			},
+
+			["Release - version 1.0.1"] = {
+				Date = "25 | 04 | 2025",
+				Header = "",
+				Content = [[
+[+] Changed MainFrame's Transparency 
+[+] Changed Font to Inter SemiBold (inspired by maclib)
+[+] Changed Keysystem Sizing and Exit Animation
+[+] Changed Notification Sizing And Positioning
+		]],
+
+			}
+		}
